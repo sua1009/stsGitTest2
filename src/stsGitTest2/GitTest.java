006a6607aee891c6b919490546039f5bb69be58c
sua1009 <sua100913@gmail.com>
2020-12-31 12:29:32 +0900

    깃허브에서 다운받은 파일

--- src/stsGitTest2/GitTest.java
+++ src/stsGitTest2/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
 		System.out.println("sts에서 git 사용 테스트1");
 		System.out.println("sts에서 git 사용 테스트 2");
 		System.out.println("sts와 sourceTree연동 테스트1");
+		System.out.println("깃허브에서 다운받아 수정한 파일");
 	}
 
 }
