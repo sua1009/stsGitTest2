a3052fdfce09456b2381503b3f8f693a1e4e12e6
sua1009 <sua100913@gmail.com>
2020-12-31 11:26:23 +0900

    gitTest.java 수정

---
+++ src/stsGitTest2/GitTest.java
@@ -0,0 +1,10 @@
+package stsGitTest2;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sts에서 git 사용 테스트1");
+	}
+
+}
