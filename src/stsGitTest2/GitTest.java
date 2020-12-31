eca91bf5f145c61d66c016d2ba405f4eef692b9c
sua1009 <sua100913@gmail.com>
2020-12-31 11:28:07 +0900

    git test수정

--- src/stsGitTest2/GitTest.java
+++ src/stsGitTest2/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sts에서 git 사용 테스트1");
+		System.out.println("sts에서 git 사용 테스트 2");
 	}
 
 }
